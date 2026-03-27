a4ec60722a4dc2ff44019a62543f8cbdfcb20798
Leo0806 <leo0806@outlook.de>
2026-03-27 07:42:56 +0100

    added missing break statements in ADD execution handler.
    minor corectness changes

--- X86_64_EMU_SOFT/HEADER/X86_64_EMU_SOFT/SYSTEM/CPU/EXECUTION_ENGINE/EXECUTION_HANDLERS/HANDLERS_ALU.h
+++ X86_64_EMU_SOFT/HEADER/X86_64_EMU_SOFT/SYSTEM/CPU/EXECUTION_ENGINE/EXECUTION_HANDLERS/HANDLERS_ALU.h
@@ -1,11 +1,17 @@
 #pragma once
+#include <bit>
+#include <cassert>
 #include <cstdint>
 #include <cstring>
 #include <HELPERS/MACROS.h>
 #include <print>
 #include <source_location>
+#include <SYSTEM/CPU/REGISTERS/GPR.h>
+#include <SYSTEM/CPU/REGISTERS/REGISTER_BASE.h>
 #include <tracy/Tracy.hpp>
 #include <tuple>
+#include <utility>
+#include <variant>
 #include "HELPERS/GLOBALS.h"
 #include "SYSTEM/CPU/EXCEPTIONS/UNDEFINED_OPCODE.h"
 #include "SYSTEM/CPU/EXECUTION_ENGINE/EXECUTION_ENGINE.h"
@@ -15,6 +21,7 @@
 namespace X86_64_EMU_SOFT::SYSTEM::CPU {
 	inline void Handle_ADD(VirtualCore& core, const INSTRUCTIONS::Instruction& instruction) {
 		ZoneScoped;//NOLINT
+		std::ignore = core;
 		if (instruction.Operand0.Type == INSTRUCTIONS::OPERANDS::OperandType::Memory || instruction.Operand2.Type == INSTRUCTIONS::OPERANDS::OperandType::Memory) {
 			throw EXCEPTIONS::UNDEFINED_OPCODE("Memory operands are not supported yet for ADD");
 		}
@@ -37,6 +44,7 @@ namespace X86_64_EMU_SOFT::SYSTEM::CPU {
 					case 64:return sourceRegister.GetValue();
 					default:NeverOrAssert(false);
 				}
+				__assume(false);
 				}();
 
 		}
@@ -49,28 +57,28 @@ namespace X86_64_EMU_SOFT::SYSTEM::CPU {
 
 		const auto& destinationOperand = std::get<INSTRUCTIONS::OPERANDS::RegisterOperand>(instruction.Operand0.Data);
 		assert(destinationOperand.Flags & std::to_underlying(INSTRUCTIONS::OPERANDS::RegisterOperandFlags::isGeneralPurposeRegister));//the generic add handler only works for gprs
-		 REGISTERS::GPR& destinationRegister = static_cast<REGISTERS::GPR&>(*std::bit_cast<REGISTERS::Register*>(destinationOperand.RegisterPointer));
-		 switch (destinationOperand.SizeBits)
-		 {
-			 case 8: {
-				 if (destinationOperand.Flags & std::to_underlying(INSTRUCTIONS::OPERANDS::RegisterOperandFlags::isHighByteRegister)) {
-					 destinationRegister.SetHigh8Bits(destinationRegister.GetHigh8Bits() + static_cast<uint8_t>(sourceVal));
-				 }
-				 else {
+		auto& destinationRegister = static_cast<REGISTERS::GPR&>(*std::bit_cast<REGISTERS::Register*>(destinationOperand.RegisterPointer));
+		switch (destinationOperand.SizeBits)
+		{
+			case 8: {
+				if (destinationOperand.Flags & std::to_underlying(INSTRUCTIONS::OPERANDS::RegisterOperandFlags::isHighByteRegister)) {
+					destinationRegister.SetHigh8Bits(destinationRegister.GetHigh8Bits() + static_cast<uint8_t>(sourceVal));
+				}
+				else {
 
-					 destinationRegister.SetLow8Bits(destinationRegister.GetLow8Bits() + static_cast<uint8_t>(sourceVal));
-				 }
-				 break;
-			 }
-			 case 16:destinationRegister.SetLow16Bits(destinationRegister.GetLow16Bits() + static_cast<uint16_t>(sourceVal));
-			 case 32:destinationRegister.SetLow32Bits(destinationRegister.GetLow32Bits() + static_cast<uint32_t>(sourceVal));
-			 case 64:destinationRegister.SetValue(destinationRegister.GetValue() + sourceVal);
-			 default:NeverOrAssert(false);
-		 }
+					destinationRegister.SetLow8Bits(destinationRegister.GetLow8Bits() + static_cast<uint8_t>(sourceVal));
+				}
+				break;
+			}
+			case 16:destinationRegister.SetLow16Bits(destinationRegister.GetLow16Bits() + static_cast<uint16_t>(sourceVal)); break;
+			case 32:destinationRegister.SetLow32Bits(destinationRegister.GetLow32Bits() + static_cast<uint32_t>(sourceVal)); break;
+			case 64:destinationRegister.SetValue(destinationRegister.GetValue() + sourceVal); break;
+			default:NeverOrAssert(false); break;
+		}
 
 
 
-		
+
 
 
 
